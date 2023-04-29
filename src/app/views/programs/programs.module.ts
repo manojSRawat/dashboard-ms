64454e9b81ea316f5b4cb351559ef9d1ba0a0384
Shruti3004 <mail2shruti.ag@gmail.com>
2023-04-29 16:02:12 +0530

    remove: tab component
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/app/views/programs/programs.module.ts
+++ src/app/views/programs/programs.module.ts
@@ -3,23 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ProgramsRoutingModule } from './programs-routing.module';
 import { ProgramsComponent } from './programs.component';
-import { TabComponent } from './tab/tab.component';
 import { CardComponent } from './card/card.component';
-import {MatTabsModule} from "@angular/material/tabs";
-import {SharedModule} from "../../shared/shared.module";
+import { MatTabsModule } from "@angular/material/tabs";
+import { SharedModule } from "../../shared/shared.module";
 
 
 @NgModule({
   declarations: [
     ProgramsComponent,
-    TabComponent,
     CardComponent
   ],
-    imports: [
-        CommonModule,
-        ProgramsRoutingModule,
-        MatTabsModule,
-        SharedModule
-    ]
+  imports: [
+    CommonModule,
+    ProgramsRoutingModule,
+    MatTabsModule,
+    SharedModule
+  ]
 })
 export class ProgramsModule { }
